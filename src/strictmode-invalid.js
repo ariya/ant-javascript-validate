936ffb9bdceae880d317788bb86fe06b61b3782e
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-11-26 18:46:14 -0800

    Example of Ant task to validate JavaScript.

---
+++ src/strictmode-invalid.js
@@ -0,0 +1,14 @@
+function f() {
+  'use strict';
+  var a = 021;
+  var b = function (eval) {}
+  var c = function (arguments) {}
+  function d(foo, foo) {}
+  function e(eval, arguments) {}
+  function eval() {}
+  function arguments() {}
+  function interface(){}
+  with (x) { }
+  try { eval++; } catch (arguments) {}
+  return { x: 1, y: 2, x: 1 }
+}
